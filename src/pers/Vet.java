5a1bce84b999fe89e4a06c0d00caae51d3d68117
Teruaki <guilherme.teruaki@gmail.com>
2020-04-13 22:52:09 +0100

    typo

--- src/pers/Vet.java
+++ src/pers/Vet.java
@@ -29,7 +29,7 @@ public class Vet extends Staff{
             return false;
                 }
             else{ 
-                return patientList.add( animalID);
+                return patientList.add(animalID);
               }
         
     
