89e9f0c9ba57007b6ee314b20150a94b6d804a05
Guilherme Teruaki <52307826+guilhermeteruaki@users.noreply.github.com>
2020-04-15 15:13:55 +0100

    Menu implemented

--- src/Factory.java
+++ src/Factory.java
@@ -8,6 +8,10 @@ import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
+import Animal.Cats;
+import Animal.Dogs;
+import Animal.Exotic;
+
 
 public class Factory{
 
@@ -59,7 +63,7 @@ public class Factory{
 
         //Create Vets
         try {  
-            System.out.println("How many Vets ?\n");
+            System.out.println("How many Vets ?");
             Scanner sc = new Scanner(System.in);
             int numVets = sc.nextInt();
                             
@@ -74,7 +78,7 @@ public class Factory{
 
                 Main.veterinarianList.put(Main.CreateStaffID(), vet);
                 vet.setStaffID(Main.staffIDCounter());
-                sc.close();
+                
                 
             } 
         }
@@ -83,9 +87,88 @@ public class Factory{
         }
     }
 
-    
+    public void createNewPet(){
+        //get list of pet names
+        ArrayList<String> listOfPetNames = new ArrayList<>();
+        ArrayList<String> listOfExoticTypes = new ArrayList<>();
+            listOfExoticTypes.add("Lizard");
+            listOfExoticTypes.add("Rabit");
+            listOfExoticTypes.add("Snake");
+            listOfExoticTypes.add("Spider");
+        ArrayList<String> listOfConditions = new ArrayList<>();
+            listOfConditions.add("Broken bone");
+            listOfConditions.add("Large Cut");
+            listOfConditions.add("Infection");
+            listOfConditions.add("Shortness of Breath");
+            listOfConditions.add("Vitamin Deficiency");
+            listOfConditions.add("Flees");
+
+        try {
+            BufferedReader BR = new BufferedReader(new FileReader("C:/Users/guilh/Documents/learning Programing/Java/Vet/src/PetNames.txt"));
+            String line = BR.readLine();
+
+            while (line != null){
+            listOfPetNames.add(line);
+            line = BR.readLine();
+            }
+            BR.close();
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File PetNames.txt not found!");
+        }
+        catch (IOException e){
+            System.out.println("IOException Pet name");
+        } 
+
+        //generate new pets
+        try {  
+            System.out.println("How many Pets ?");
+            Scanner sc = new Scanner(System.in);
+            int numPets = sc.nextInt();
+            Random random = new Random();                
+            for (int i = 0; i < (numPets/3); i++) {
+                
+                
+                // get a random arguments for the new Pet
+                Cats cat = new Cats(listOfPetNames.get(random.nextInt(listOfPetNames.size())),
+                                      "Cat",
+                                      random.nextInt(15), 
+                                      listOfConditions.get(random.nextInt(listOfConditions.size())));
+
+                // Vet vet = new Vet(listOfFirstNames.get(random.nextInt(listOfFirstNames.size())),
+                //                   listOfMiddleNames.get(random.nextInt(listOfMiddleNames.size())),
+                //                   random.nextDouble()*10, 
+                //                   listOfSpecialities.get(random.nextInt(listOfSpecialities.size())));
+
+                // Main.veterinarianList.put(Main.CreateStaffID(), vet);
+                // vet.setStaffID(Main.staffIDCounter());
+                // sc.close();
+                
+            } 
+
+            for (int i=(numPets/3); i< (2*numPets/3); i++){
+
+                Dogs dog = new Dogs(listOfPetNames.get(random.nextInt(listOfPetNames.size())),
+                                    "Dog",
+                                    random.nextInt(20),
+                                    listOfConditions.get(random.nextInt(listOfConditions.size())));
+            }
+
+            for (int i=(2*numPets/3); i<numPets; i++){
+
+                Exotic exotic = new Exotic(listOfPetNames.get(random.nextInt(listOfPetNames.size())),
+                                            "Exotic",
+                                            random.nextInt(10),
+                                            listOfConditions.get(random.nextInt(listOfConditions.size())),
+                                            listOfExoticTypes.get(random.nextInt(listOfExoticTypes.size())));
+            }
+            sc.close();
+        }
+        catch (InputMismatchException e) {
+            System.out.println("That's not a valid Number.");
+        }
 
     
-
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 //import Animal.*;
 import pers.*;
 
@@ -31,15 +34,69 @@ public class Main {
        public static HashMap<Long, Object> AnimalList = new HashMap<Long, Object>();
 
        public static void main(String[] args) {
+              boolean exitProgram = false;
+              do {
+                     try{
+                            Scanner sc = new Scanner(System.in);
+                            System.out.println("Please choose from:\n" 
+                                                 + "1 - Generate New Veterinarian.\n"
+                                                 + "2 - Generate New Animal.\n" 
+                                                 + "3 - List All Vets.\n" 
+                                                 + "4 - \n"
+                                                 + "999 - To exit the program.");
+                            int menuChoice = sc.nextInt();
+                            
 
-            Factory factory = new Factory();
-              factory.createNewVet();
+                            switch (menuChoice) {
+                                   case 1:
+                                   System.out.println("\n");
+                                          Factory factory = new Factory();
+                                          factory.createNewVet();
+                                          System.out.println("\n");
+                                          break;
+                            
+                                          
+                                   case 2:
+                                          System.out.println("\n");
+                                          System.out.println("test 2");
+                                          System.out.println("\n");
+                                          break;
+                                   case 3:
+                                          System.out.println("\n");
+                                          veterinarianList.entrySet().forEach(entry -> {
+                                                 System.out.println("Vet ID: " + entry.getKey() +
+                                                 " Vet Name: " + ((Vet) entry.getValue()).getStaffName());
+                                          });
+                                          System.out.println("\n");
+                                          break;
+                                          
+                                   case 4:
+                                          System.out.println("\n");
+                                          System.out.println("test 4");
+                                          System.out.println("\n");
+                                          break;
 
-              veterinarianList.entrySet().forEach(entry->{
-                     System.out.println("Vet ID: " + entry.getKey()
-                     + " Vet Name: " + ((Vet) entry.getValue()).getStaffName());
-       });
-                     
+                                   case 999:
+                                          exitProgram = true;
+                                          break;
+
+                                   default:
+                                   System.out.println("That's not a valid option!");
+                                   System.out.println("\n");
+                                          break;
+                            }
+                            
+                     }             
+                      catch (InputMismatchException e) {
+                            System.out.println("That's not a valid Number.");
+                            System.out.println("\n");
+                     }
+                           
+
+               
+              }
+              while (exitProgram == false);
+              
        }
 
 
