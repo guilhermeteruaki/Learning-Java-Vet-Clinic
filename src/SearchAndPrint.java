53da46688adf492c36f9c44ae28dc2d49da1ea8b
Guilherme Teruaki <52307826+guilhermeteruaki@users.noreply.github.com>
2020-05-11 12:51:06 +0100

    Removed Atomic Ref

--- src/SearchAndPrint.java
+++ src/SearchAndPrint.java
@@ -12,20 +12,18 @@ only the later one will be displayed. simple problem to fix. too much trouble to
         
         System.out.println("The name of the veterinarian?\n Use first name or Last name");
         String nameToSearch = sc.nextLine();
-        //https://stackoverflow.com/questions/30026824/modifying-local-variable-from-inside-lambda
-        AtomicReference<String> printable = new AtomicReference<>();
-
+      
         Main.veterinarianList.entrySet().forEach(entry ->{
             if ( ((Vet) entry.getValue()).getStaffFirstName().toLowerCase().equals(nameToSearch.toLowerCase()) || 
                                 ((Vet) entry.getValue()).getStaffLastName().toLowerCase().equals(nameToSearch.toLowerCase())){
-                                    printable.set("Vet ID: " + entry.getKey() +
+                                  System.out.println("Vet ID: " + entry.getKey() +
                                                     ", Vet Name: " + ((Vet) entry.getValue()).getStaffName()
                                                     + ", Vet speciality: " + ((Vet) entry.getValue()).getMedicalSpeciality()
                                                     + ", Vet salary: " +(((Vet) entry.getValue()).getSalary())+"\n");}
                     
 
         });
-        System.out.println(printable);
+  
         //sc.close();
     }
 
@@ -33,18 +31,17 @@ only the later one will be displayed. simple problem to fix. too much trouble to
         
         System.out.println("The name of the Staff\n Use first name or Last name");
         String nameToSearch = sc.nextLine();
-        //https://stackoverflow.com/questions/30026824/modifying-local-variable-from-inside-lambda
-        AtomicReference<String> printable = new AtomicReference<>();
+        
 
         Main.AdminList.entrySet().forEach(entry ->{
             if ( ((Admin) entry.getValue()).getStaffFirstName().toLowerCase().equals(nameToSearch.toLowerCase()) || 
                                 ((Admin) entry.getValue()).getStaffLastName().toLowerCase().equals(nameToSearch.toLowerCase())){
-                    printable.set("Staff ID: " + entry.getKey() 
+                    System.out.println("Staff ID: " + entry.getKey() 
                                  +", Staff Name: " + ((Admin) entry.getValue()).getStaffName() 
                                  + ", Staff Sector: " + ((Admin) entry.getValue()).getAdminSector()
                                  + ", The Staff is currently "+ ((Admin) entry.getValue()).getAdminJob());}
      });
-        System.out.println(printable);
+        
         // sc.close();
     }
 
@@ -52,18 +49,16 @@ only the later one will be displayed. simple problem to fix. too much trouble to
         
         System.out.println("The name of the  animal\n");
         String nameToSearch = sc.nextLine();
-        //https://stackoverflow.com/questions/30026824/modifying-local-variable-from-inside-lambda
-        AtomicReference<String> printable = new AtomicReference<>();
-
+       
         Main.AnimalList.entrySet().forEach(entry -> {
             if ( ((Animals) entry.getValue()).getAnimalName().toLowerCase().equals(nameToSearch.toLowerCase())){
-                    printable.set("Animal ID: " + entry.getKey() 
+                    System.out.println("Animal ID: " + entry.getKey() 
                     +", Animal Name: " + ((Animals) entry.getValue()).getAnimalName() 
                     + ", Animal type: " + ((Animals) entry.getValue()).getAnimalType()
                     +", Animal Age: " + ((Animals) entry.getValue()).getAnimalAge()
                     + ", Medical Condition: " + ((Animals) entry.getValue()).getMedicalCond());}
      });
-        System.out.println(printable);
+        
         // sc.close();
     }
 }
